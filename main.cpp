af04e0621a4034bb741635763f69e4889746d51e
Brayanalvarez07 <160290621+Brayanalvarez07@users.noreply.github.com>
2024-03-02 01:31:53 +0000

    Version 4

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 #include <Mascota.hpp>
 #include <Alimento.hpp>
+#include <Dibujo.hpp>
 int main(int argc, char const *argv)
 {
+        Dibujo dibujo("./assets/kokoa.txt");
+        Dibujo dibujo2("./assets/dog.txt");
+        dibujo.Dibujar();
+        dibujo2.Dibujar();
+
         Mascota m1;
         Mascota m2;
         Mascota m3;
@@ -13,6 +19,6 @@ int main(int argc, char const *argv)
         m1.Comer(alimento);
         m2.Comer(alimento);
         m3.Comer(alimento);
-        cout <<"Mascota" <<m1.LeerEnergia()<<endl;
+        cout<<"Mascota" <<m1.LeerEnergia()<<endl;
 
 }
